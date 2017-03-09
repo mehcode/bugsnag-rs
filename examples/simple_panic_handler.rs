985d76bfa8f7891c002b0b29d0fe183ab7297f53
Bastian Köcher <git@kchr.de>
2017-03-09 16:21:17 +0100

    Removes globalinstance as part of the library
    - Moves the simple_panic_handler into a selft contained rs file

---
+++ examples/simple_panic_handler.rs
@@ -0,0 +1,40 @@
+extern crate bugsnag_api;
+#[macro_use]
+extern crate lazy_static;
+
+use bugsnag_api::{bugsnag, stacktrace};
+
+use std::panic;
+
+// The panic handler consumes the api object, so no further modifications to
+// the object are possible
+fn register_panic_handler(api: bugsnag::Bugsnag) {
+    panic::set_hook(Box::new(move |info| {
+        let message = match info.payload().downcast_ref::<&str>() {
+            Some(msg) => msg,
+            None => "unknown error!",
+        };
+
+        let stacktrace = stacktrace::create_stacktrace(api.get_project_source_dir());
+
+        if api.notify("Panic",
+                    message,
+                    bugsnag::Severity::Error,
+                    &stacktrace,
+                    None)
+            .is_err() {
+            println!("Error at notifying bugsnag!");
+        }
+    }));
+}
+
+fn main() {
+    let mut api = bugsnag::Bugsnag::new("api-key", Some(env!("CARGO_MANIFEST_DIR")));
+    api.set_app_info(Some(env!("CARGO_PKG_VERSION")),
+                     Some("development"),
+                     Some("rust"));
+
+    register_panic_handler(api);
+
+    panic!("Hello from a Rust panic!");
+}
